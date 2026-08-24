54e4ae48b0357b9feda0001f1480624f730497c5
StarAwA117 <starawa10001@outlook.com>
2026-08-24 18:19:38 +0800

    增加 WebUI 动画

--- mod/notify/main.js
+++ mod/notify/main.js
@@ -1,11 +1,11 @@
 export default class Notify {
 	onClientConnect(client, isMainClient) {
 		const role = isMainClient ? "主客户端" : "客户端";
-		client.tell(`§e${this.config.ws.name} | §fSystem > §i${role} 已连接`);
+		client.tell(`§e${this.config.ws.name} | §fSystem > §i${role}已连接`);
 	}
 
 	onClientDisconnect(client, isMainClient) {
 		const role = isMainClient ? "主客户端" : "客户端";
-		client.tell(`§e${this.config.ws.name} | §fSystem > §i${role} 已断开连接`);
+		client.tell(`§e${this.config.ws.name} | §fSystem > §i${role}已断开连接`);
 	}
 }
